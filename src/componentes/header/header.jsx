3c407fecca4f5d18194eaf9c154c2b97671279bf
Diego Fernando <diego_maximo160@hotmail.com>
2024-04-04 16:57:50 -0300

    Criado componente de icone

--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -1,6 +1,7 @@
 import Title from '../title/Title'
 import Subtitle from '../subtitle/subtitle'
 import estilos from './header.module.css'
+import Icon from '../icon/icon'
 
 
 function Header () {
@@ -8,6 +9,9 @@ function Header () {
         <div className={estilos.header}>
             <Title>Jogo da Velha</Title>
             <Subtitle>Criado por Diego Fernando</Subtitle>
+            <div className={estilos.iconContent}>
+                <Icon iconName="github" link="https://github.com/DFmaximo" />
+            </div>
         </div>
     )
 }
